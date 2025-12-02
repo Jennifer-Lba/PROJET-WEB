ca742c94b81c49bdfd737421907dab94c22b9c66
Kifouly <davidkifouly22@gmail.com>
2025-12-02 18:46:51 +0100

    Ajout de AdminController.php

---
+++ controllers/AdminController.php
@@ -0,0 +1,59 @@
+<?php
+session_start();
+
+require_once __DIR__ . '/../helpers/functions.php';
+require_once __DIR__ . '/../model/User.php';
+require_once __DIR__ . '/../model/Quiz.php';
+
+class AdminController
+{
+    private static function requireAdmin()
+    {
+        if (!isset($_SESSION['user']) || $_SESSION['user']['role'] !== 'admin') {
+            redirect('/view/auth/login.php');
+            exit;
+        }
+    }
+
+    public static function dashboard()
+    {
+        self::requireAdmin();
+
+        $users = User::getAll();
+        $quizzes = Quiz::getAll();
+
+        include __DIR__ . '/../view/admin/dashboard.php';
+    }
+
+    public static function deleteUser($id)
+    {
+        self::requireAdmin();
+        User::delete($id);
+        redirect('/view/admin/dashboard.php');
+    }
+
+    public static function deleteQuiz($id)
+    {
+        self::requireAdmin();
+        Quiz::delete($id);
+        redirect('/view/admin/dashboard.php');
+    }
+}
+
+if (isset($_GET['action'])) {
+    session_start();
+
+    require_once __DIR__ . '/../model/User.php';
+    require_once __DIR__ . '/../model/Quiz.php';
+    require_once __DIR__ . '/../helpers/functions.php';
+
+    $action = $_GET['action'];
+
+    if ($action === 'deleteUser' && isset($_GET['id'])) {
+        AdminController::deleteUser($_GET['id']);
+    }
+
+    if ($action === 'deleteQuiz' && isset($_GET['id'])) {
+        AdminController::deleteQuiz($_GET['id']);
+    }
+}
